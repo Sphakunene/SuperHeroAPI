303bda03e00881f8ff5a5d2167ee1ec169d2fe71
Tshimangadzo <mudzanani27@gmail.com>
2023-05-12 13:27:02 +0200

    my first commit

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Model;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : Controller
+    {
+        private static List<SuperHero> superHero = new List<SuperHero> {
+
+            new SuperHero { Id = 1,
+                Name = "Spider Man",
+                FirstName="Peter",
+                LastName= "Parker",
+                Place="New York City"
+            },
+            new SuperHero { Id = 2,
+                Name = "BatMan",
+                FirstName="Paul",
+                LastName= "Smith",
+                Place="Dubai"
+            },
+            new SuperHero { Id = 3,
+                Name = "Goku",
+                FirstName="Son",
+                LastName= "Yew",
+                Place="bangkok"
+            }
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> getAllHeroes() {
+            return Ok(superHero);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> getAllHero(int id)
+        {
+            var hero = superHero.Find(x => x.Id == id);
+
+            if (hero is null) { 
+                return NotFound("Sorry the hero is not found");
+            }
+            return Ok(hero);
+
+        }
+
+
+    }
+}
